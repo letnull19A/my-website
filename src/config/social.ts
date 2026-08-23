c3e966d77f9ca8dd58c32b985ac57b2473c6234b
da_b1rmuda <lamurka1337@yandex.ru>
2026-08-23 23:23:48 +0400

    feat(config): Обновил ссылки на соц сети

--- src/config/social.ts
+++ src/config/social.ts
@@ -8,10 +8,10 @@ export interface SocialLink {
 export const email = 'letnull19a@gmail.com';
 
 export const socialLinks: SocialLink[] = [
-  { id: 'github', label: 'github', href: 'https://github.com', iconSrc: '/icons/github.svg' },
-  { id: 'linkedin', label: 'linkedin', href: 'https://linkedin.com', iconSrc: '/icons/linkedin.svg' },
-  { id: 'telegram', label: 'telegram', href: 'https://t.me', iconSrc: '/icons/telegram.svg' },
-  { id: 'whatsapp', label: 'whatsapp', href: 'https://wa.me', iconSrc: '/icons/whatsapp.svg' },
+  { id: 'github', label: 'github', href: 'https://github.com/letnull19a', iconSrc: '/icons/github.svg' },
+  { id: 'linkedin', label: 'linkedin', href: 'https://www.linkedin.com/in/aleksei-volkov-572ba9401/', iconSrc: '/icons/linkedin.svg' },
+  { id: 'telegram', label: 'telegram', href: 'https://t.me/alexei_wolkoff', iconSrc: '/icons/telegram.svg' },
+  { id: 'whatsapp', label: 'whatsapp', href: 'https://wa.me/995511706127', iconSrc: '/icons/whatsapp.svg' },
   { id: 'email', label: 'email', href: `mailto:${email}`, iconSrc: '/icons/email.svg' },
 ];
 
